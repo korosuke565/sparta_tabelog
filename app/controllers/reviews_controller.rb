bd4c850eccbe17722f460feba384c0e50b2f7de3
korosuke565 <t12619tt@sfc.keio.ac.jp>
2015-06-27 18:39:08 +0900

    newとcreate周りの実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,31 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(
+      restaurant_id: params[:restaurant_id],
+      user_id: current_user.id,
+      body: review_params["body"],
+      score: review_params["score"]
+    )
+    if @review.save
+      redirect_to restaurant_url(@review.restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :body,
+      :score
+      )
+  end
+
 end
