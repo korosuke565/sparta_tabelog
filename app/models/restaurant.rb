ee7de8bf5edf1f9d7002b13e4a237461f49529a4
korosuke565 <t12619tt@sfc.keio.ac.jp>
2015-06-27 17:43:47 +0900

    reviewコントローラの作成

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ActiveRecord::Base
+  has_many :reviews
+  has_many :users, through: :reviews
+
   validates :store_name, presence: true
   validates :tel,        presence: true
   validates :address,    presence: true
