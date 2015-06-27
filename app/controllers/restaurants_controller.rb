b8c7bbc9350b959a065198f47fade80da36ef42c
korosuke565 <t12619tt@sfc.keio.ac.jp>
2015-06-27 15:40:55 +0900

    newとcreateメソッドの実装

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,9 +3,16 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +25,16 @@ class RestaurantsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(
+      :store_name,
+      :tel,
+      :address,
+      :website
+    )
   end
 end
